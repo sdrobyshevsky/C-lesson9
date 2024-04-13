d46d39428228698b0c8d62a5cdc4b5a70f42b418
sdrobyshevsky <sdrobyshevsky@mail.ru>
2024-04-13 23:45:48 +0300

    Added info about task

--- obj/Debug/net8.0/Task11.AssemblyInfo.cs
+++ obj/Debug/net8.0/Task11.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task11")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7311bce2a36253aac7a1b236707d898f87b98cbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task11")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task11")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
